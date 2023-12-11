80e197b2132a69fb6db6d64cc4ed3f5ed740a41f
andreas <andsek@hotmail.com>
2023-12-11 20:30:08 +0100

    oppdaterer palindrome

--- palindromeNumber/palindromeNumber.py
+++ palindromeNumber/palindromeNumber.py
@@ -5,12 +5,16 @@ def pali_num(num: int) -> bool:
 	print(list_of_digits)
 	for i in range(len(list_of_digits)):
 		if(i>(len(list_of_digits)-1)-i):
+			print("palindrome")
 			return
-		if list_of_digits[i] == list_of_digits[(len(list_of_digits)-1)-i]:
-			print("is pali")
-		else:
-			print("not pali")
+		if list_of_digits[i] != list_of_digits[(len(list_of_digits)-1)-i]:
+			print("not palindrome")
 			return
+	print("palindrome")
 
 
 pali_num(123321)
+pali_num(123456)
+pali_num(5656565)
+pali_num(2580852)
+pali_num(12)
